c125513c24b6c6a66be0223293531d837bece750
duniyash <80444721+duniyash@users.noreply.github.com>
2025-04-01 17:29:19 +0530

    Refactor imports in page.tsx to streamline component usage
    - Removed unused useEffect import from React in page.tsx to clean up the code and improve readability.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, ChangeEvent, useRef, useEffect } from "react";
+import React, { useState, ChangeEvent, useRef } from "react";
 import MusicSheet from "../components/MusicSheet";
 import { Button } from "@/components/ui/button"; // adjust import path as needed
 
